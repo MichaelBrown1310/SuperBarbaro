c1287ae03026145b26844391029a8eee1e5a95f9
juan-zapata12 <juan.zapata12@usc.edu.co>
2026-03-13 20:27:02 -0500

    Vistas
    Se crearon las vistas Reportes, Ventas y Inventario

---
+++ SuperBarbaro/src/views/Ventas/VentasPage.vue
@@ -0,0 +1,28 @@
+<template>
+
+<ion-page>
+
+<AppHeader titulo="VENTAS" />
+
+<ion-content class="fondo">
+
+</ion-content>
+
+</ion-page>
+
+</template>
+
+<script setup>
+
+import { IonPage, IonContent } from '@ionic/vue'
+import AppHeader from '../../components/AppHeader.vue'
+
+</script>
+
+<style>
+
+.fondo{
+--background:white;
+}
+
+</style>
