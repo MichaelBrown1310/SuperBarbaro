5a184484bbba3c00f237fbe727dabb4ae22b584e
MichaelBrown1310 <michaelmoreno1310@gmail.com>
2026-03-13 20:16:46 -0500

    Vista Usuarios
    Se crea la vista de la lista de los usuarios y del registro de los ususarios

---
+++ SuperBarbaro/src/views/usuarios/RegistrarUsuarioPage.vue
@@ -0,0 +1,302 @@
+<template>
+
+<ion-page>
+
+<ion-header>
+
+<ion-toolbar class="toolbar">
+
+<ion-buttons slot="start">
+<ion-button @click="volver">
+<ion-icon :icon="chevronBackOutline"></ion-icon>
+</ion-button>
+</ion-buttons>
+
+<ion-title class="titulo">
+REGISTRAR USUARIO
+</ion-title>
+
+</ion-toolbar>
+
+</ion-header>
+
+<ion-content class="fondo">
+
+<div class="contenedor">
+
+<input
+type="file"
+ref="inputFoto"
+accept="image/*"
+style="display:none"
+@change="cargarFoto"
+/>
+
+<div class="foto" @click="abrirArchivos">
+
+<img v-if="foto" :src="foto">
+
+<ion-icon v-else :icon="person" class="icono"></ion-icon>
+
+</div>
+
+<input class="input" placeholder="Código" v-model="codigo"/>
+
+<input class="input" placeholder="Nombre" v-model="nombre"/>
+<input class="input" placeholder="Apellido" v-model="apellido"/>
+<input class="input" placeholder="Cédula" v-model="cedula"/>
+
+<input class="input" placeholder="Correo" v-model="correo"/>
+<input class="input" placeholder="Teléfono" v-model="telefono"/>
+<input class="input" placeholder="Dirección" v-model="direccion"/>
+
+<select v-model="rol" class="input">
+
+<option disabled value="">Seleccionar rol</option>
+
+<option value="CAJERO">Cajero</option>
+<option value="COCINERO">Cocinero</option>
+
+</select>
+
+<input class="input" type="password" placeholder="Contraseña" v-model="password"/>
+
+<button class="boton" @click="registrar">
+REGISTRAR
+</button>
+
+</div>
+
+</ion-content>
+
+</ion-page>
+
+</template>
+
+
+<script setup>
+
+import {
+IonPage,
+IonHeader,
+IonToolbar,
+IonTitle,
+IonContent,
+IonButtons,
+IonButton,
+IonIcon
+} from '@ionic/vue'
+
+import {
+person,
+chevronBackOutline
+} from 'ionicons/icons'
+
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const codigo = ref('')
+const nombre = ref('')
+const apellido = ref('')
+const cedula = ref('')
+const correo = ref('')
+const telefono = ref('')
+const direccion = ref('')
+const rol = ref('')
+const password = ref('')
+
+const foto = ref(null)
+
+const inputFoto = ref(null)
+
+const abrirArchivos = ()=>{
+
+inputFoto.value.click()
+
+}
+
+const cargarFoto = (event)=>{
+
+const file = event.target.files[0]
+
+if(file){
+
+foto.value = URL.createObjectURL(file)
+
+}
+
+}
+
+const registrar = async () => {
+
+if(
+!codigo.value ||
+!nombre.value ||
+!apellido.value ||
+!password.value ||
+!rol.value
+){
+
+alert("Debes llenar los campos obligatorios")
+return
+
+}
+
+try{
+
+const response = await fetch('http://localhost:3000/usuarios',{
+
+method:'POST',
+
+headers:{
+'Content-Type':'application/json'
+},
+
+body:JSON.stringify({
+
+codigo:codigo.value,
+password:password.value,
+nombre:nombre.value,
+apellido:apellido.value,
+cedula:cedula.value,
+direccion:direccion.value,
+correo:correo.value,
+telefono:telefono.value,
+rol:rol.value,
+foto:foto.value
+
+})
+
+})
+
+const data = await response.json()
+
+if(data.success){
+
+alert("Usuario registrado")
+
+router.push('/usuarios')
+
+}else{
+
+alert("Error registrando usuario")
+
+}
+
+}catch(error){
+
+console.log(error)
+
+}
+
+}
+
+const volver = ()=>{
+
+router.push('/usuarios')
+
+}
+
+</script>
+
+
+<style>
+
+.fondo{
+--background:white;
+}
+
+.toolbar{
+--background:white;
+border-bottom:2px solid black;
+}
+
+.contenedor{
+
+display:flex;
+flex-direction:column;
+align-items:center;
+
+margin-top:20px;
+
+}
+
+.foto{
+
+width:100px;
+height:100px;
+
+border-radius:50%;
+
+background:black;
+
+display:flex;
+align-items:center;
+justify-content:center;
+
+margin-bottom:25px;
+
+overflow:hidden;
+
+cursor:pointer;
+
+}
+
+.foto img{
+
+width:100%;
+height:100%;
+object-fit:cover;
+
+}
+
+.icono{
+
+font-size:50px;
+color:white;
+
+}
+
+.input{
+
+width:85%;
+
+padding:10px;
+
+border:none;
+
+border-bottom:2px solid black;
+
+margin-bottom:20px;
+
+font-size:16px;
+
+background:white;
+
+color:black;
+
+}
+
+.boton{
+
+width:85%;
+
+padding:12px;
+
+border:2px solid black;
+
+border-radius:25px;
+
+background:white;
+
+color:black;
+
+font-size:16px;
+
+cursor:pointer;
+
+}
+
+</style>
---
+++ SuperBarbaro/src/views/usuarios/UsuariosPage.vue
@@ -0,0 +1,297 @@
+<template>
+
+<ion-page>
+
+<ion-header>
+<ion-toolbar class="toolbar">
+
+<ion-buttons slot="start">
+<ion-button @click="volver">
+<ion-icon :icon="chevronBackOutline"></ion-icon>
+</ion-button>
+</ion-buttons>
+
+<ion-title class="titulo">
+USUARIOS
+</ion-title>
+
+</ion-toolbar>
+</ion-header>
+
+<ion-content class="fondo">
+
+<div class="lista">
+
+<div
+class="usuario"
+v-for="u in usuarios"
+:key="u.codigo"
+>
+
+<div class="izquierda">
+
+<div class="foto-usuario">
+
+<img v-if="u.foto" :src="'http://localhost:3000/' + u.foto"/>
+
+<ion-icon
+v-else
+:icon="person"
+class="icono"
+/>
+
+</div>
+
+<div class="info">
+
+<p class="rol">
+{{ u.rol }}
+</p>
+
+<p class="nombre">
+{{ u.nombre }} {{ u.apellido }}
+</p>
+
+</div>
+
+</div>
+
+<ion-button
+fill="clear"
+class="editar"
+@click="editarUsuario(u)"
+>
+<ion-icon :icon="createOutline"></ion-icon>
+</ion-button>
+
+</div>
+
+</div>
+
+<div class="contenedor-boton">
+
+<button class="boton-registrar" @click="registrarUsuario">
+REGISTRAR USUARIO
+</button>
+
+</div>
+
+</ion-content>
+
+</ion-page>
+
+</template>
+
+
+
+<script setup>
+
+import {
+IonPage,
+IonHeader,
+IonToolbar,
+IonTitle,
+IonContent,
+IonIcon,
+IonButtons,
+IonButton
+} from '@ionic/vue'
+
+import {
+person,
+createOutline,
+chevronBackOutline
+} from 'ionicons/icons'
+
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const usuarios = ref([])
+
+onMounted(async ()=>{
+
+try{
+
+const response = await fetch('http://localhost:3000/usuarios')
+
+const data = await response.json()
+
+usuarios.value = data
+
+}catch(error){
+
+console.log("Error cargando usuarios")
+
+}
+
+})
+
+const volver = () => {
+
+router.push('/tabs/perfil')
+
+}
+
+const editarUsuario = (usuario) => {
+
+console.log("editar", usuario)
+
+}
+
+const registrarUsuario = () => {
+
+router.push('/registrar-usuario')
+
+}
+
+</script>
+
+
+
+<style>
+
+.fondo{
+--background:white;
+}
+
+.toolbar{
+--background:white;
+border-bottom:2px solid black;
+}
+
+.lista{
+
+width:100%;
+
+display:flex;
+flex-direction:column;
+align-items:center;
+
+margin-top:10px;
+
+}
+
+.usuario{
+
+width:90%;
+max-width:500px;
+
+display:flex;
+justify-content:space-between;
+align-items:center;
+
+border:2px solid black;
+border-radius:15px;
+
+padding:12px 15px;
+
+margin:10px 0;
+
+}
+
+.izquierda{
+
+display:flex;
+align-items:center;
+
+}
+
+.foto-usuario{
+
+width:35px;
+height:35px;
+
+min-width:35px;
+
+border-radius:50%;
+
+background:black;
+
+display:flex;
+align-items:center;
+justify-content:center;
+
+margin-right:12px;
+
+overflow:hidden;
+
+}
+
+.foto-usuario img{
+
+width:100%;
+height:100%;
+object-fit:cover;
+
+}
+
+.icono{
+
+font-size:16px;
+color:white;
+
+}
+
+.info{
+
+display:flex;
+flex-direction:column;
+
+color:black;
+
+}
+
+.rol{
+
+font-weight:bold;
+text-transform:uppercase;
+
+font-size:12px;
+
+margin:0;
+
+}
+
+.nombre{
+
+font-size:13px;
+
+margin:0;
+
+}
+
+.editar{
+
+color:black;
+
+}
+
+.contenedor-boton{
+
+display:flex;
+justify-content:center;
+
+margin-top:25px;
+
+}
+
+.boton-registrar{
+
+width:90%;
+max-width:500px;
+
+padding:12px;
+
+border:2px solid black;
+border-radius:25px;
+
+background:white;
+color:black;
+
+font-size:16px;
+
+}
+
+</style>
