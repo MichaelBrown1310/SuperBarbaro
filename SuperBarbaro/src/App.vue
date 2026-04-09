45dbc83ce08829081fe9c8dc8c6066edf8bd3d67
ValeriaGonzalezE <valeriagoes170805@gmail.com>
2026-04-09 14:21:47 -0500

    alertas en inventario
    Se añadieron las alertas en productos del inventario, según el mínimo requerido de cada insumo en la tabla de productos de la base de datos, las genera en cualquier pantalla, exceptuando el login y tiene un sonido beep al iniciar, el mensaje quedara ahí siempre a menos de que se cierre o haya los insumos mínimos requeridos y al recargar pagina volverán a aparecer los que cerro y siguen sin tener el mínimo necesario.

--- SuperBarbaro/src/App.vue
+++ SuperBarbaro/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <ion-app>
+    <AlertasStock />
     <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import AlertasStock from '@/components/AlertasStock.vue'
 </script>
